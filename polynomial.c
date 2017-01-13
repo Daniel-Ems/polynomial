f9227fc709a17526da2d64ee6592f4822c09f31b
Daniel <danielems3@outlook.com>
2017-01-13 15:06:19 -0500

    Add a remove zeros function to handle if a coefficient gets zerod out

--- polynomial.c
+++ polynomial.c
@@ -42,28 +42,43 @@ void poly_print(const polynomial *eqn)
 	poly_print(eqn->next);
 }
 
+
 char *poly_to_string(polynomial *p) 
 //Returns a newely-malloced string that displays the given polynomial
 {
+	//two buffers: one to hold each snprintf return and then the other to 
+	//return. They are split to better control the amount of data written 
+	//to the buffer and prevent any buffer overflows.
 	char *tmp = malloc(64);	
 	char *string = calloc(256,1);
-	struct term *cursor = p;
-	//struct term *previous = cursor;
+
+	struct term *cursor = p; //Set equal to p to start at the beginnign of the 
+							 //Polynomial.
+
 	int count=0;
 	while(cursor)
 	{
 	
-		//printf("second loop iteration%s\n", tmp);
+		//c and e are used as variables that hold the coeff and exp of each node
 		int c = cursor->coeff;
 		int e = cursor->exp;
-		//previous = cursor;
+		
+		//the values for the current cursor position have already been stored
+		//which is why the cursor can move before the if statement begins.
+		/* Think about possibly moving this to the bottom? */
 		cursor = cursor->next;
+		
+		//There are only four possibilities for C, which can be simplified
+		//in two if statements. >= && <=
 		if(c >= 1)
 		{
 			if(e == 1)
 			{
+			//count holds the number of bites written and is set up to 
+			//add to itself. This gives the programmer the ability to use
+			//count in combination with the return buffer to check the 
+			//buffer size 
 			count += snprintf(tmp, 64, c == 1 ? "+%dX " : "+%dX ",c);
-			//printf("%s\n", tmp);
 			}
 			else if(e > 1)
 			{
@@ -97,29 +112,50 @@ char *poly_to_string(polynomial *p)
 	return string;
 }
 
-void Simplify_poly(polynomial **a)
+
+void remove_zeros(polynomial *poly)
 {
 	struct term *cursor, *previous;
-	cursor = *a;
+	cursor = poly;
+	previous = cursor;
+	while(cursor)
+	{
+		if(cursor->coeff == 0)
+		{
+			previous->next = cursor->next;
+			free(cursor);
+			cursor = previous->next;
+		}
+		else{
+			previous = cursor;
+			cursor = cursor->next;
+		}
+	}
+}
+
+void Simplify_poly(polynomial *a)
+{
+	struct term *cursor, *previous;
+	cursor = a;
 	previous = cursor;
 	cursor = cursor -> next;
+
 	while(cursor)
 	{
 		if(previous->exp == cursor->exp)
 		{
-			previous->coeff += cursor -> coeff;
-			//printf("%d", previous->coeff);
+			previous->coeff += cursor->coeff;
 			previous -> next = cursor->next;
 			free(cursor);
-			cursor = previous->next;
+			cursor = previous -> next;					
 		}
 		else
 		{
-		previous = cursor;
-		cursor = cursor->next;
+			previous = cursor;
+			cursor = cursor->next;
 		}
 	}
-	cursor = *a;
+	remove_zeros(a);
 }
 			
 //polynomial *poly_add(const polynomial *a, const polynomial *b)
@@ -140,21 +176,36 @@ void Simplify_poly(polynomial **a)
 int main()
 {
 
-struct term *front= term_create(-1, 2);
-front->next = term_create(5, 2);
+struct term *front= term_create(-1, 5);
+front->next = term_create(5, 5);
 front->next->next = term_create(3, 2);
-//front->next = second;
-//second->next = third;
+front->next->next->next=term_create(-3,2);
 polynomial *poly = front;
 poly_print(poly);
+puts(" ");
+Simplify_poly(poly);
 char *string = poly_to_string(poly);
-puts("test");
 printf("%s\n", string);
-Simplify_poly(&poly);
 poly_print(poly);
-poly_destroy(poly);
-//free(poly);
+puts(" ");
+
+struct term *second = term_create(1,4);
+second->next = term_create(2,3);
+second->next->next=term_create(-2,3);
+polynomial *polyB = second;
+poly_print(polyB);
+puts(" ");
+Simplify_poly(polyB);
+char *stringB = poly_to_string(polyB);
+printf("%s\n",stringB);
+puts(" ");
+
+
+free(stringB);
+poly_destroy(polyB);
 free(string);
+poly_destroy(poly);
+
 }
 
 
