4e46805c7d34ee2e4d1f699800209b06dbdd0316
Daniel <danielems3@outlook.com>
2017-01-13 09:51:57 -0500

    Finalize string function, Still need to comment and perform error checking

--- polynomial.c
+++ polynomial.c
@@ -49,11 +49,46 @@ char *poly_to_string(polynomial *p)
 	int count=0;
 	while(cursor)
 	{
+		//printf("second loop iteration%s\n", tmp);
 		int c = cursor->coeff;
 		int e = cursor->exp;
 		//previous = cursor;
 		cursor = cursor->next;
-		count = snprintf(tmp+count, 64, c > 1 ? "+%dx^%d " : "%dx^%d ", c,e);
+		if(c == 1 || c == -1)
+		{
+			if(e == 1)
+			{
+			count = snprintf(tmp+count, 64, c == 1 ? "+%dX " : "%dX ",c);
+			//printf("%s\n", tmp);
+			}
+			else if(e > 1)
+			{
+			count = snprintf(tmp+count, 64, c == 1 ? "+X^%d " : "-X^%d ", e);
+			}
+			else
+			{ 
+			count = snprintf(tmp+count, 64, c == 1 ? "+%d " : "%d ",c); 
+			}
+		}
+		if(c > 1 || c < 0)
+		{
+			if(e == 1)
+			{
+			count = snprintf(tmp+count, 64, c > 1 ? "+%dX " : "%dX ", c);
+			}
+			else if(e > 1)
+			{
+			count = snprintf(tmp+count, 64, c > 1 ? "+%dX^%d " : "%dX^%d ",c, e);
+			//printf("%d, %d\n", c, count);
+			}
+			else
+			{ 
+			count = snprintf(tmp+count, 64, c > 1 ? "+%d ": "%d ",c); 
+			}
+			
+		}
+	
+		
 	}
 	return tmp;
 }
@@ -75,8 +110,8 @@ char *poly_to_string(polynomial *p)
 int main()
 {
 
-struct term *front= term_create(-3, 5);
-struct term *second = term_create(3, 4);
+struct term *front= term_create(-1, 1);
+struct term *second = term_create(5, 2);
 front->next = second;
 polynomial *poly = front;
 poly_print(poly);
