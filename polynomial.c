dc63e0fc5fc35ac7dcd66bd4b18064038e355255
Daniel <danielems3@outlook.com>
2017-01-13 11:52:06 -0500

    Create a simplify function and fix some parts of my string function

--- polynomial.c
+++ polynomial.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "polynomial.h"
 
@@ -45,53 +46,82 @@ char *poly_to_string(polynomial *p)
 //Returns a newely-malloced string that displays the given polynomial
 {
 	char *tmp = malloc(64);	
+	char *string = calloc(256,1);
 	struct term *cursor = p;
+	//struct term *previous = cursor;
 	int count=0;
 	while(cursor)
 	{
+	
 		//printf("second loop iteration%s\n", tmp);
 		int c = cursor->coeff;
 		int e = cursor->exp;
 		//previous = cursor;
 		cursor = cursor->next;
-		if(c == 1 || c == -1)
+		if(c >= 1)
 		{
 			if(e == 1)
 			{
-			count = snprintf(tmp+count, 64, c == 1 ? "+%dX " : "%dX ",c);
+			count += snprintf(tmp, 64, c == 1 ? "+%dX " : "+%dX ",c);
 			//printf("%s\n", tmp);
 			}
 			else if(e > 1)
 			{
-			count = snprintf(tmp+count, 64, c == 1 ? "+X^%d " : "-X^%d ", e);
+			count += snprintf(tmp, 64, c == 1 ? "+%dX^%d " : "+%dX^%d ",c,e);
 			}
 			else
 			{ 
-			count = snprintf(tmp+count, 64, c == 1 ? "+%d " : "%d ",c); 
+			count += snprintf(tmp, 64, c == 1 ? "+%d " : "%d ",c); 
 			}
 		}
-		if(c > 1 || c < 0)
+		if(c <= -1)
 		{
 			if(e == 1)
 			{
-			count = snprintf(tmp+count, 64, c > 1 ? "+%dX " : "%dX ", c);
+			count += snprintf(tmp, 64, c == -1 ? "%dX " : "%dX ", c);
 			}
 			else if(e > 1)
 			{
-			count = snprintf(tmp+count, 64, c > 1 ? "+%dX^%d " : "%dX^%d ",c, e);
+			count += snprintf(tmp, 64, c == -1 ? "%dX^%d " : "%dX^%d ",c, e);
 			//printf("%d, %d\n", c, count);
 			}
 			else
 			{ 
-			count = snprintf(tmp+count, 64, c > 1 ? "+%d ": "%d ",c); 
+			count += snprintf(tmp, 64, c == -1 ? "%d ": "%d ",c); 
 			}
-			
 		}
-	
-		
+	strncat(string, tmp, 256-count);
+	//cursor = cursor->next;
 	}
-	return tmp;
+	free(tmp);
+	return string;
 }
+
+void Simplify_poly(polynomial **a)
+{
+	struct term *cursor, *previous;
+	cursor = *a;
+	previous = cursor;
+	cursor = cursor -> next;
+	while(cursor)
+	{
+		if(previous->exp == cursor->exp)
+		{
+			previous->coeff += cursor -> coeff;
+			//printf("%d", previous->coeff);
+			previous -> next = cursor->next;
+			free(cursor);
+			cursor = previous->next;
+		}
+		else
+		{
+		previous = cursor;
+		cursor = cursor->next;
+		}
+	}
+	cursor = *a;
+}
+			
 //polynomial *poly_add(const polynomial *a, const polynomial *b)
 //Returns a newly-malloced polynomial that is the sum of the two arguments
  
@@ -110,15 +140,21 @@ char *poly_to_string(polynomial *p)
 int main()
 {
 
-struct term *front= term_create(-1, 1);
-struct term *second = term_create(5, 2);
-front->next = second;
+struct term *front= term_create(-1, 2);
+front->next = term_create(5, 2);
+front->next->next = term_create(3, 2);
+//front->next = second;
+//second->next = third;
 polynomial *poly = front;
 poly_print(poly);
 char *string = poly_to_string(poly);
-puts(" ");
+puts("test");
 printf("%s\n", string);
-
+Simplify_poly(&poly);
+poly_print(poly);
+poly_destroy(poly);
+//free(poly);
+free(string);
 }
 
 
