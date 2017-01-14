81a0b0d48033493cbbc369d06173ed2c3bd866a4
Daniel <danielems3@outlook.com>
2017-01-13 19:05:28 -0500

    Add the declarations to the .h fil

--- polynomial.h
+++ polynomial.h
@@ -10,4 +10,12 @@ typedef struct term {
 
 //typedef struct term polynomial;
 
+struct term *term_create(int coeff, int exp);
+void Simplify_poly(polynomial *a);
+void remove_zeros(polynomial **poly);
+char *poly_to_string(polynomial *p);
+void poly_print(const polynomial *eqn);
+void poly_destroy(polynomial *eqn);
+polynomial *poly_add(polynomial *a, polynomial *b);
+polynomial *poly_sub(polynomial *a, polynomial *b);
 
